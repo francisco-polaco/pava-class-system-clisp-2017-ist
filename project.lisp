23a830de80a25a9ffbef3c4fd1ffc823b45f4e5b
Francisco Santos <franciscopolaco@ist.utl.pt>
2017-05-18 11:13:16 +0100

    struture

---
+++ project.lisp
@@ -0,0 +1,154 @@
+(defvar table-fields (make-hash-table))
+(defvar table-inheritance (make-hash-table))
+(defvar table-index (make-hash-table))
+(defvar table-size (make-hash-table))
+
+;(defmacro while (test &body body)
+ ;   ((do ()
+  ;      ((not ,test))
+   ;     ,@body)))
+
+(defun turn-to-list (l)
+    (cond ((not (listp l)) (list l))
+        (t l)))
+
+(defun list-to-2d-array (list)
+  (map 'array #'identity list))  
+  
+(defun unnest (x)
+  (labels ((rec (x acc)
+    (cond ((null x) acc)
+      ((atom x) (cons x acc))
+      (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+  
+(defun get-batata (x)
+    (print "yey")
+    (vector (symbol-name x)  (unnest (gethash x table-fields)))) 
+  
+(defun has_inheritance (lst) (not (equal (list-length lst) 1)))
+
+(defun size-fields (cl) (list-length (gethash cl table-fields)))
+
+(defun get-class-to-tb-defined (l) (car l))
+
+(defun get-all-keyargs (class-list)
+    (let ((keyargs nil))
+        (dolist (el (cdr class-list) keyargs) 
+            (setf keyargs (append keyargs (gethash el table-fields)))
+            (setf keyargs (append keyargs (get-all-keyargs (gethash el table-inheritance))))
+            )))
+            
+(defun get-keyargs-of-class (cl)
+    (gethash cl table-fields))
+            
+(defun get-supers (cl) (gethash cl table-inheritance))
+
+(defun get-full-size (cl)
+    (let ((acum (size-fields cl)))
+        (dolist (c (get-supers cl) acum)
+            (setf acum (+ acum (get-full-size c))))))
+            
+(defun get-index-class (cl inh-cl)
+    (let ((pos 0)
+            (cnt -1))
+    (dolist (c (gethash cl table-inheritance))
+        (incf cnt)
+        (cond ((equal c inh-cl) (setf pos cnt))))
+    (nth pos (gethash cl table-index))))
+            
+(defmacro def-class (classname &rest args)
+ (let ((puta 1)
+        (class-list (turn-to-list classname))
+        (all-keyword-args args))
+        
+    ; set fields from a class to table
+    (setf (gethash (get-class-to-tb-defined class-list) table-fields) args) 
+    ;(print "args")(print args)
+    
+    ; writing #fields table
+    (setf (gethash (get-class-to-tb-defined class-list) table-size) (size-fields (get-class-to-tb-defined class-list)))
+    
+    ;inheritance table ready
+    (setf (gethash (get-class-to-tb-defined class-list) table-inheritance) (cdr class-list))
+    
+    (setf (gethash (get-class-to-tb-defined class-list) table-index) nil)
+    ; getting all keyword args ready && index ready
+    (cond ((has_inheritance class-list) (setf all-keyword-args (append all-keyword-args (get-all-keyargs class-list)))
+                                        (let ((list-index-inh (list (1+ (list-length args))))
+                                                (acum (1+ (list-length args))))
+                                            (dolist (super (cddr class-list))
+                                                (setf acum (+ acum (gethash super table-size)))
+                                                (append list-index-inh (list acum)))
+                                            (setf (gethash (get-class-to-tb-defined class-list) table-index) list-index-inh))))
+    (print all-keyword-args)
+    `(progn
+
+        ;make
+        (defun ,(intern (concatenate 'string "MAKE-"
+                                    (symbol-name (get-class-to-tb-defined class-list))))
+                        (&key ,@all-keyword-args)
+            
+            (vector ,(symbol-name (get-class-to-tb-defined class-list)) ,@all-keyword-args ))
+
+        
+        ;getClass
+        (defun ,(intern "GET-CLASS") (object)
+            (aref object 0))
+        
+        ;instance TODO
+        (defun ,(intern (concatenate 'string (symbol-name (get-class-to-tb-defined class-list)) "?")) (object)
+            (cond ((arrayp object) 
+                    (cond ((equal (car (array-dimensions object)) (+ (list-length ',args) 1)) 
+                    (equalp ,(symbol-name (get-class-to-tb-defined class-list)) (aref object 0)))
+                    ('t nil)))
+                ('t nil)))
+                
+        
+        ;getters       
+        ,(dolist (el all-keyword-args)
+            ;(print (symbol-name el))
+            ;(print (symbol-name (get-class-to-tb-defined class-list)))
+            (eval `(defun ,(intern (concatenate 'string (symbol-name (get-class-to-tb-defined class-list)) "-" (symbol-name el))) (object)
+                (let ((cls-name (get-class object))
+                        (index 0))
+                (print cls-name)
+                (cond ((not (equal cls-name ,(symbol-name (get-class-to-tb-defined class-list)))) 
+                            (print "special code")
+                            (setf index (get-index-class (intern cls-name) ',(get-class-to-tb-defined class-list)))
+                            (aref object (1- (+ ,puta index))))
+                      ('t (print "regular code") (aref object ,puta))))))
+            (incf puta))
+        )))
+
+        ;'(cond ((> puta (size-fields (get-class-to-be-defined class-list)))))
+                ;`(,(intern (concatenate 'string (symbol-name (get-class-to-tb-defined class-list)) "-CLASS")) object)
+        
+(print "PERSON CLASS")
+(def-class person age name)
+(defvar p (make-person :age 10 :name "ola"))
+(print "IST CLASS")
+(def-class (ist person) id)
+(defvar i (make-ist :id 1 :age 20 :name "matos"))
+;(print "PHD CLASS")
+;(def-class (phd ist) thesis)
+;(defvar phd (make-phd :thesis "opah" :id 13 :age 21 :name "ler"))
+
+
+; (defmacro def-class12 (&body args)
+;  (setf symbol (gensym "mysymbol"))
+;   (setf symbol (make-hash-table-fields))
+;   (setf (gethash "ola" (car args)) (car (rest args)))
+;   )
+
+; (defmacro def-class (x)
+;  `(setf ab ,x))
+;
+; (defmacro def-class (&body args)
+; `(progn
+;    (defvar ,(car args) nil)
+;    (setf ,(car args) '(,(cdar args)))))
+; (defun flatten (l)
+;   (cond ((null l) nil)
+;         ((atom (car l)) (cons (car l) (flatten (cdr l))))
+;         (t (append (flatten (car l)) (flatten (cdr l))))))
